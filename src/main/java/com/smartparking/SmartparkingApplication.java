c2a3b70fcdd865ec2111e3270e6b1af6097bdd12
Abdelrahman Shaaban <abdelrahmanshaaban125@gmail.com>
2026-02-28 03:52:53 +0200

    Initial commit

--- src/main/java/com/smartparking/SmartparkingApplication.java
+++ src/main/java/com/smartparking/SmartparkingApplication.java
@@ -1,29 +1,15 @@
 package com.smartparking;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.smartparking.allocation.Slot;
-import com.smartparking.allocation.SlotRepository;
 
 @SpringBootApplication
 public class SmartparkingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartparkingApplication.class, args);
-	}
-	@Bean
-	CommandLineRunner initSlots(SlotRepository repo) {
-		return args -> {
-			if (repo.count() == 0) {
-				repo.save(new Slot(null, "A1", true, null));
-				repo.save(new Slot(null, "A2", true, null));
-				repo.save(new Slot(null, "B1", true, null));
-			}
-		};
-
 	}
 
 }
